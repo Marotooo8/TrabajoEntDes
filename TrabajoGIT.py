6db8f78d6c74ed56058ca9d7ae64b374f4f96ca0
AzureAD\MarcosMarotoGranados <2021351@iesluisbraille.es>
2022-01-27 09:23:44 +0100

    VERSION 1.0

---
+++ TrabajoGIT.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 25 11:02:38 2021
+
+@author: MarcosMarotoGranados
+"""
+
+opc=0
+while(opc!=1):  # con este while controlamos que el programa se ejecute hasta que metamos "Salir -> 8"
+    print("1.-Suma")
+    opc = int(input("Intro opc válida:"))
+    while(opc<1 or opc>1):  # con este while controlamos que la opción esté en el rango correcto
+        opc = int(input("Intro opc válida:"))
+    
+
+    if(opc==1): 
+        x=int(input("Introduzca el primer número: "))
+        y=int(input("Introduzca el segundo número: "))
+        print(f"{x}+{y}={x+y}")
+    else:
+        print("ERROR")
+                
