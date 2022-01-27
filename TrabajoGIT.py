c14e27ff8033da404af78fa6f63f919f11967bbb
AzureAD\MarcosMarotoGranados <2021351@iesluisbraille.es>
2022-01-27 09:51:09 +0100

    Version 4.0

--- TrabajoGIT.py
+++ TrabajoGIT.py
@@ -6,13 +6,14 @@ Created on Mon Oct 25 11:02:38 2021
 """
 
 opc=0
-while(opc!=4):  # con este while controlamos que el programa se ejecute hasta que metamos "Salir -> 8"
+while(opc!=5):  # con este while controlamos que el programa se ejecute hasta que metamos "Salir -> 8"
     print("1.-Suma")
     print("2.-Resta")
     print("3.-Multiplicación")
-    print("4.-Salir")
+    print("4.-División")
+    print("5.-Salir")
     opc = int(input("Intro opc válida:"))
-    while(opc<1 or opc>4):  # con este while controlamos que la opción esté en el rango correcto
+    while(opc<1 or opc>5):  # con este while controlamos que la opción esté en el rango correcto
         opc = int(input("Intro opc válida:"))
     
 
@@ -31,6 +32,14 @@ while(opc!=4):  # con este while controlamos que el programa se ejecute hasta qu
         y=int(input("Introduzca el segundo número: "))
         print(f"{x}*{y}={x*y}")
         
+    elif(opc==4):
+        x=int(input("Introduzca el primer número: "))
+        y=int(input("Introduzca el segundo número: "))
+        print(f"{x}/{y}={x/y}")
+        if(y==0):
+            print("0 es un numero valido")
+        else:
+            print("El resto es: ")
     else:
         print("ERROR")
                 
