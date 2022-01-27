cceab39a84a504c3aa24b96ec1248c975ec53ada
AzureAD\MarcosMarotoGranados <2021351@iesluisbraille.es>
2022-01-27 10:10:02 +0100

    Version 7.0

--- TrabajoGIT.py
+++ TrabajoGIT.py
@@ -6,16 +6,17 @@ Created on Mon Oct 25 11:02:38 2021
 """
 
 opc=0
-while(opc!=7):  # con este while controlamos que el programa se ejecute hasta que metamos "Salir -> 8"
+while(opc!=8):  # con este while controlamos que el programa se ejecute hasta que metamos "Salir -> 8"
     print("1.-Suma")
     print("2.-Resta")
     print("3.-Multiplicación")
     print("4.-División")
     print("5.-Cociente")
     print("6.-Resto")
-    print("7.-Salir")
+    print("7.-Exponencia de dos números")
+    print("8.-Salir")
     opc = int(input("Intro opc válida:"))
-    while(opc<1 or opc>7):  # con este while controlamos que la opción esté en el rango correcto
+    while(opc<1 or opc>8):  # con este while controlamos que la opción esté en el rango correcto
         opc = int(input("Intro opc válida:"))
     
 
@@ -60,6 +61,14 @@ while(opc!=7):  # con este while controlamos que el programa se ejecute hasta qu
             print("0 es un numero valido")
         else:
             print("El resto es: ")
+            
+    elif(opc==7):
+         x=int(input("Introduzca el primer número: "))
+         y=int(input("Introduzca el segundo número: "))
+         print(f"{x}*{x}={x*x}")
+         print(f"{y}*{y}={y*y}")
+         
     else:
         print("Salir")
+        
                 
